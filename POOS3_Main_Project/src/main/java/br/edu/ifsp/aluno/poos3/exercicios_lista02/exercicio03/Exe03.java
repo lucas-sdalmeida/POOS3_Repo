77862d81c7b7f8ebbdde7cfb1d6df50937df8118
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2022-12-25 20:53:38 -0300

    Criação Arquivo do Exercício 0203
    Criação do arquivo .java do exercício 03 da lista 02 e adição de um exemplo de entradas e saídas no arquivo de descrição correspondente.
    
    O exercício não foi resolvido, mas já foram declaradas as principais variáveis e métodos previstos para serem usados.

---
+++ POOS3_Main_Project/src/main/java/br/edu/ifsp/aluno/poos3/exercicios_lista02/exercicio03/Exe03.java
@@ -0,0 +1,31 @@
+package br.edu.ifsp.aluno.poos3.exercicios_lista02.exercicio03;
+
+import java.util.Scanner;
+
+public class Exe03 {
+    public static final int ARRAY_LENGTH = 10;
+    public static final int ARRAY_INITIAL_LENGTH = 5;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[ARRAY_LENGTH];
+        int arrayNextIndex = 0;
+        int number;
+    }
+
+    public static int indexOf(int[] array, int lastIndex, int number) {
+        return -1;
+    }
+
+    public static boolean addInArray(int[] array, int lastIndex, int number) {
+        return false;
+    }
+
+    public static boolean removeFromArray(int[] array, int index) {
+        return false;
+    }
+
+    public static void printArray(int[] array, int lastIndex) {
+
+    }
+}
