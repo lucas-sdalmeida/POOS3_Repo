087c2e78e925abe1ded2abf04c0c26de99dd2c3d
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2023-01-08 18:34:56 -0300

    Criação da Classe do Exe 0308

---
+++ POOS3_Main_Project/src/main/java/br/edu/ifsp/aluno/poos3/exercicios_lista03/exercicio08/Exe08.java
@@ -0,0 +1,17 @@
+/*
+Descrição do Exercício
+
+Professor: Lucas Bueno Ruas de Oliveira
+
+Simule 100.000 jogadas com dois dados de seis lados e, na sequência, imprima as
+estatísticas (em porcentagem) para cada valor de resultado de soma possível. O
+resultado era o esperado inicialmente? Não? Por quê?
+*/
+
+package br.edu.ifsp.aluno.poos3.exercicios_lista03.exercicio08;
+
+public class Exe08 {
+    public static void main(String[] args) {
+
+    }
+}
